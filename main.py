199f73488694232b1e90c9a60644d2424395ad47
harisuryaa <harisurya119@gmail.com>
2022-09-18 00:10:15 +0530

    change SQLite to Postgres

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(256), nullable=False)
 
     posts = relationship('Post', back_populates='author')
 
