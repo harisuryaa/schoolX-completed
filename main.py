cfd54ccb6cb478c0670e034273083808b3d0b549
harisuryaa <harisurya119@gmail.com>
2022-09-17 23:49:39 +0530

    change SQLite to Postgres

--- main.py
+++ main.py
@@ -26,8 +26,9 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
+
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  'sqlite:///schoolX_5.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
