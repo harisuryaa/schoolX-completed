121830dee06e15a3d1794f43c6fea30b89ef380d
harisuryaa <harisurya119@gmail.com>
2022-09-29 23:32:10 +0530

    change to local data base sql

--- main.py
+++ main.py
@@ -22,12 +22,12 @@ ADMINS= [1]
 edit = False
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('sqlite:///schoolX_5.db')
+app.config['SQLALCHEMY_DATABASE_URI']= "sqlite:///scholx_new.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -235,4 +235,4 @@ def logout():
     return redirect(url_for('search'))
 
 if __name__ == "__main__":
-    app.run(host='172.20.10.3')
+    app.run()
